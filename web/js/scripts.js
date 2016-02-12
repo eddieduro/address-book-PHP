41f0fb4ad0b7723262397bc8887c56558a73f42a
eddie <e.jamesduro@gmail.com>
2016-02-12 13:18:56 -0800

    sliding contact info reveal and hide

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
 	$('.toggleInfo').click(function(){
 		// $('.hideme').toggle();
-		$(this).children('li').toggle();
+		$(this).children('li').slideToggle('slow');
+		 $('html, body').animate({
+	      scrollTop: $(this).offset().top + $('window').height()
+	    }, 1000);
 	});
 
 });
