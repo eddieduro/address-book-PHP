ef9ea7636cf701656b9bcf90eabed3a7863f8d49
eddie <e.jamesduro@gmail.com>
2016-02-12 09:18:32 -0800

    add root route

---
+++ app/app.php
@@ -0,0 +1,18 @@
+<?php 
+	require_once __DIR__.'/../vendor/autoload.php';
+	require_once __DIR__.'/../src/Contact.php';
+
+	session_start();
+	if(empty($_SESSION['list_of_contacts'])){
+		$_SESSION['list_of_contacts'] = array();
+	}
+
+	$app = new Silex\Application();
+	$app->request(new Silex\Provider\TwigServiceProvider, array(
+		'twig.path'=> __DIR__.'/../views'
+	));
+
+	$app->get('/', function() use ($app) {
+		return $app['twig']->render('address_book.html.twig');
+	});
+?>
